216be20e60166ade43e2d45bc2134ae6bb537f23
blinman <savakoal@gmail.com>
2020-06-17 00:21:30 +0300

    Updated to include eslint with AirBnB styling
    installed AirBnB styling for ESlint and applied most of the recommendations

--- mytestapp.js
+++ mytestapp.js
@@ -1,21 +1,28 @@
-'use strict'
-//initial requires for operations
+// initial requires for operations
 const fs = require('fs');
 const request = require('request');
 const calculations = require('./calculations');
 
-//retrieves everything from the 3rd parameter onwards
+// retrieves everything from the 3rd parameter onwards
 const transDataFile = process.argv.slice(2);
 
-//sets global variables to store settings from API
+// sets global variables to store settings from API
 let cashInSettings = {};
 let cashOutSettingsLegal = {};
 let cashOutSettingsNatural = {};
 
-//function to the file with transactions
+// function to display fees
+function displayFee(amount) {
+  if (!isNaN(amount)) {
+    amount = amount.toFixed(2); // if the fee is a number as expected, convert it to string with fixed 2 decimal points
+  }
+  process.stdout.write(amount + '\n'); // displays the fee or whatever status the transaction returned
+}
+
+// function to the file with transactions
 function readTransFile() {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(transDataFile[0], function (err, data) {
+  return new Promise(((resolve, reject) => {
+    fs.readFile(transDataFile[0], (err, data) => {
       if (err) {
         reject('failed to read file' + '\n' + err);
       } else {
@@ -26,13 +33,13 @@ function readTransFile() {
         }
       };
     });
-  });
+  }));
 }
 
-//function for data from API retrieval
+// function for data from API retrieval
 function retrieveSettingsFromAPI(url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, response, body) {
+  return new Promise(((resolve, reject) => {
+    request(url, (error, response, body) => {
       if (!error && response.statusCode == 200) {
         try {
           resolve(JSON.parse(body));
@@ -43,58 +50,51 @@ function retrieveSettingsFromAPI(url) {
         reject('failed to get commision setting' + '\n' + response.statusCode);
       }
     });
+  }));
+}
+
+// generates fee depending on transactional data
+function feeGenerator(singleTransData) {
+  if (singleTransData.operation.currency != 'EUR') { // checks if currency is EUR, as only EUR transactions are permitted
+    return ('only transaction in EUR are supported' + '\n' + 'transaction info=> date: ' + singleTransData.date + ' user ID: ' + singleTransData.user_id + ' currency: ' + singleTransData.operation.currency); // if the transaction is in EUR, inform the user
+  } if (singleTransData.type == 'cash_in') {
+    return calculations.feeForCashIn(singleTransData, cashInSettings);
+  } if (singleTransData.type == 'cash_out' && singleTransData.user_type == 'juridical') {
+    return calculations.feeForCashOutLegal(singleTransData, cashOutSettingsLegal);
+  } if (singleTransData.type == 'cash_out' && singleTransData.user_type == 'natural') {
+    return calculations.feeForCashOutNatural(singleTransData, cashOutSettingsNatural);
+  } // if the transaction did not satisfy any expected parameter settings, inform the user
+  return ('unsupported transaction or user type' + '\n' + 'transaction info=> date: ' + singleTransData.date + ' user ID: ' + singleTransData.user_id + ' transaction type: ' + singleTransData.type + ' user type: ' + singleTransData.user_type);
+}
+
+// simplied main logic to work with data from file
+function mainLogic(transData) {
+  transData.forEach((singleTransData) => { // for each entry in the data
+    const resultingFee = feeGenerator(singleTransData); // generate a fee based in the transactional data
+    displayFee(resultingFee); // display the fee in the required format
   });
 }
 
-//starts the whole transaction fee counting process
-if (transDataFile.length != 0) { //if the 3rd parameter (presumably transaction file part) was given, start the program
-  let promises = [];
+// starts the whole transaction fee counting process
+if (transDataFile.length !== 0) { // if the 3rd parameter (presumably transaction file part) was given, start the program
+  const promises = [];
   promises.push(retrieveSettingsFromAPI('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-in'));
   promises.push(retrieveSettingsFromAPI('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/juridical'));
   promises.push(retrieveSettingsFromAPI('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural'));
 
-  Promise.all(promises).then(function (retrievedSettings) { //requests settings from APIs concurrently
+  Promise.all(promises).then((retrievedSettings) => { // requests settings from APIs concurrently
     cashInSettings = retrievedSettings[0];
     cashOutSettingsLegal = retrievedSettings[1];
     cashOutSettingsNatural = retrievedSettings[2];
-  }).then(function () { //once all the API have been read and settings stored, read the transactions file
-    return readTransFile(); //retruns file contents
-  }).then(function (dataFromFile) { //once the file has been read start working with the data
+  }).then(() => // once all the API have been read and settings stored, read the transactions file
+    // eslint-disable-next-line implicit-arrow-linebreak
+    readTransFile(), // retruns file contents
+  ).then((dataFromFile) => { // once the file has been read start working with the data
     mainLogic(dataFromFile);
-  }).catch(function (error) {
-    process.stdout.write(error);
   })
+    .catch((error) => {
+      process.stdout.write(error);
+    });
 } else {
-  process.stdout.write('have not received the filepath' + '\n'); //if no argument were submitted when launching the program, inform the user
-}
-
-//simplied main logic to work with data from file
-function mainLogic(transData) {
-  transData.forEach(function (singleTransData) { //for each entry in the data
-    let resultingFee = feeGenerator(singleTransData); //generate a fee based in the transactional data
-    displayFee(resultingFee); //display the fee in the required format
-  });
-}
-
-//generates fee depending on transactional data
-function feeGenerator(singleTransData) {
-  if (singleTransData.operation.currency != 'EUR') { //checks if currency is EUR, as only EUR transactions are permitted
-    return ('only transaction in EUR are supported' + '\n' + 'transaction info=> date: ' + singleTransData.date + ' user ID: ' + singleTransData.user_id + ' currency: ' + singleTransData.operation.currency); //if the transaction is in EUR, inform the user
-  } else if (singleTransData.type == 'cash_in') {
-    return calculations.feeForCashIn(singleTransData, cashInSettings);
-  } else if (singleTransData.type == 'cash_out' && singleTransData.user_type == 'juridical') {
-    return calculations.feeForCashOutLegal(singleTransData, cashOutSettingsLegal);
-  } else if (singleTransData.type == 'cash_out' && singleTransData.user_type == 'natural') {
-    return calculations.feeForCashOutNatural(singleTransData, cashOutSettingsNatural);
-  } else { //if the transaction did not satisfy any expected parameter settings, inform the user
-    return ('unsupported transaction or user type' + '\n' + 'transaction info=> date: ' + singleTransData.date + ' user ID: ' + singleTransData.user_id + ' transaction type: ' + singleTransData.type + ' user type: ' + singleTransData.user_type);
-  }
-}
-
-//function to display fees
-function displayFee(amount) {
-  if (!isNaN(amount)) {
-    amount = amount.toFixed(2); //if the fee is a number as expected, convert it to string with fixed 2 decimal points
-  }
-  process.stdout.write(amount + '\n'); //displays the fee or whatever status the transaction returned
+  process.stdout.write('have not received the filepath\n'); // if no argument were submitted when launching the program, inform the user
 }
--- test/calculations.test.js
+++ test/calculations.test.js
@@ -1,51 +1,51 @@
-//initial requires for test and function operations
+// initial requires for test and function operations
 const expect = require('chai').expect;
 const calculations = require('../calculations');
 
-//dummy API settings
+// dummy API settings
 const cashInSettings = {
-    "percents": 0.03,
-    "max": {
-        "amount": 5
-    }
+  "percents": 0.03,
+  "max": {
+    "amount": 5
+  }
 };
 
 const cashOutSettingsLegal = {
-    "percents": 0.3,
-    "min": {
-        "amount": 0.5
-    }
+  "percents": 0.3,
+  "min": {
+    "amount": 0.5
+  }
 };
 
 const cashOutSettingsNatural = {
-    "percents": 0.3,
-    "week_limit": {
-        "amount": 1000
-    }
+  "percents": 0.3,
+  "week_limit": {
+    "amount": 1000
+  }
 };
 
-//dummy transactions
+// dummy transactions
 const mockTransactions = [
-    { "date": "2015-12-29", "user_id": 1, "operation": { "amount": 200.00 } },
-    { "date": "2016-01-02", "user_id": 2, "operation": { "amount": 300.00 } },
-    { "date": "2016-01-03", "user_id": 1, "operation": { "amount": 30000 } },
-    { "date": "2016-01-07", "user_id": 1, "operation": { "amount": 1000.00 } },
-    { "date": "2016-01-07", "user_id": 1, "operation": { "amount": 100.00 } },
-    { "date": "2016-01-10", "user_id": 1, "operation": { "amount": 100.00 } },
-    { "date": "2016-01-10", "user_id": 2, "operation": { "amount": 1000000.00 } },
-    { "date": "2016-01-10", "user_id": 3, "operation": { "amount": 1000.00 } },
-    { "date": "2016-02-15", "user_id": 1, "operation": { "amount": 300.00 } },
-    { "date": "2016-02-16", "user_id": 1, "operation": { "amount": 50.00 } },
-    { "date": "2016-02-17", "user_id": 1, "operation": { "amount": 20.28 } },
-    { "date": "2016-02-18", "user_id": 1, "operation": { "amount": 638.29 } }
-]
+  { "date": "2015-12-29", "user_id": 1, "operation": { "amount": 200.00 } },
+  { "date": "2016-01-02", "user_id": 2, "operation": { "amount": 300.00 } },
+  { "date": "2016-01-03", "user_id": 1, "operation": { "amount": 30000 } },
+  { "date": "2016-01-07", "user_id": 1, "operation": { "amount": 1000.00 } },
+  { "date": "2016-01-07", "user_id": 1, "operation": { "amount": 100.00 } },
+  { "date": "2016-01-10", "user_id": 1, "operation": { "amount": 100.00 } },
+  { "date": "2016-01-10", "user_id": 2, "operation": { "amount": 1000000.00 } },
+  { "date": "2016-01-10", "user_id": 3, "operation": { "amount": 1000.00 } },
+  { "date": "2016-02-15", "user_id": 1, "operation": { "amount": 300.00 } },
+  { "date": "2016-02-16", "user_id": 1, "operation": { "amount": 50.00 } },
+  { "date": "2016-02-17", "user_id": 1, "operation": { "amount": 20.28 } },
+  { "date": "2016-02-18", "user_id": 1, "operation": { "amount": 638.29 } }
+];
 
-//dummy input parameters
+// dummy input parameters
 const percentages = 0.4;
-const currencyForRounding = [0.8423, 1.23, 120, 4, 0.422, 0.4, 4000.9999, 4, 1.20000001, 0.21, 0.08112, 2.55316]
+const currencyForRounding = [0.8423, 1.23, 120, 4, 0.422, 0.4, 4000.9999, 4, 1.20000001, 0.21, 0.08112, 2.55316];
 const currentWeeklyTotals = [200, 300, 30200, 31200, 900, 950, 1000300, 1000, 300, 350, 370.28, 1008.57];
 
-//arrays of expected results
+// arrays of expected results
 const cashInExpectations = [0.06, 0.09, 5.00, 0.30, 0.03, 0.03, 5.00, 0.30, 0.09, 0.02, 0.01, 0.20];
 const cashOutExpectationsLegal = [0.60, 0.90, 90.00, 3.00, 0.50, 0.50, 3000.00, 3.00, 0.90, 0.50, 0.50, 1.92];
 const cashOutExpectationsNatural = [0.00, 0.00, 87.60, 0.00, 0.30, 0.30, 2997, 0.00, 0.00, 0.00, 0.00, 0.03];
@@ -54,92 +54,92 @@ const roundToSmallestCurrencyItemExpectations = [0.85, 1.23, 120, 4, 0.43, 0.4, 
 const weeklyApplicableAmountExpectations = [0, 0, 30000, 1000, 0, 50, 1000000, 1000, 0, 0, 0, 638.29];
 const currentWeeklyCashOutTotalExpectations = [0, 0, 200, 0, 1000, 1100, 0, 0, 0, 300, 350, 370.28];
 
-//tests, test names are pretty self-explanatory
-describe('Calculations test', () => {
-    feeForCashInTest();
-    feeForCashOutLegalTest();
-    feeForCashOutNaturalTest();
-    percentageCalculationTest();
-    currencyRoundingTest();
-    applicableForFeeAmountTest();
-    weeklyCashOutAmountTest();
-});
-
 function feeForCashOutNaturalTest() {
-    describe('cash out test for natural', () => {
-        for (let i = 0; i < mockTransactions.length; i++) {
-            it('should return ' + cashOutExpectationsNatural[i], () => {
-                const result = calculations.feeForCashOutNatural(mockTransactions[i], cashOutSettingsNatural);
-                expect(result).to.equal(cashOutExpectationsNatural[i]);
-            });
-        }
-    });
+  describe('cash out test for natural', () => {
+    for (let i = 0; i < mockTransactions.length; i += 1) {
+      it(`should return ${cashOutExpectationsNatural[i]}`, () => {
+        const result = calculations.feeForCashOutNatural(mockTransactions[i], cashOutSettingsNatural);
+        expect(result).to.equal(cashOutExpectationsNatural[i]);
+      });
+    }
+  });
 }
 
 function feeForCashInTest() {
-    describe('cash in test', () => {
-        for (let i = 0; i < mockTransactions.length; i++) {
-            it('should return ' + cashInExpectations[i], () => {
-                const result = calculations.feeForCashIn(mockTransactions[i], cashInSettings);
-                expect(result).to.equal(cashInExpectations[i]);
-            });
-        }
-    });
+  describe('cash in test', () => {
+    for (let i = 0; i < mockTransactions.length; i += 1) {
+      it(`should return ${cashInExpectations[i]}`, () => {
+        const result = calculations.feeForCashIn(mockTransactions[i], cashInSettings);
+        expect(result).to.equal(cashInExpectations[i]);
+      });
+    }
+  });
 }
 
 function feeForCashOutLegalTest() {
-    describe('cash out test for legal', () => {
-        for (let i = 0; i < mockTransactions.length; i++) {
-            it('should return ' + cashOutExpectationsLegal[i], () => {
-                const result = calculations.feeForCashOutLegal(mockTransactions[i], cashOutSettingsLegal);
-                expect(result).to.equal(cashOutExpectationsLegal[i]);
-            });
-        }
-    });
+  describe('cash out test for legal', () => {
+    for (let i = 0; i < mockTransactions.length; i += 1) {
+      it(`should return ${cashOutExpectationsLegal[i]}`, () => {
+        const result = calculations.feeForCashOutLegal(mockTransactions[i], cashOutSettingsLegal);
+        expect(result).to.equal(cashOutExpectationsLegal[i]);
+      });
+    }
+  });
 }
 
 function percentageCalculationTest() {
-    describe('test for percentage calculation', () => {
-        for (let i = 0; i < mockTransactions.length; i++) {
-            it('should return ' + percentCalcExpectations[i], () => {
-                const result = calculations.percentageCalc(mockTransactions[i].operation.amount, percentages);
-                expect(result).to.equal(percentCalcExpectations[i]);
-            });
-        }
-    });
+  describe('test for percentage calculation', () => {
+    for (let i = 0; i < mockTransactions.length; i += 1) {
+      it(`should return ${percentCalcExpectations[i]}`, () => {
+        const result = calculations.percentageCalc(mockTransactions[i].operation.amount, percentages);
+        expect(result).to.equal(percentCalcExpectations[i]);
+      });
+    }
+  });
 }
 
 function currencyRoundingTest() {
-    describe('test for currency rounding', () => {
-        for (let i = 0; i < mockTransactions.length; i++) {
-            it('should return ' + roundToSmallestCurrencyItemExpectations[i], () => {
-                const result = calculations.roundToSmallestCurrencyItem(currencyForRounding[i]);
-                expect(result).to.equal(roundToSmallestCurrencyItemExpectations[i]);
-            });
-        }
-    });
+  describe('test for currency rounding', () => {
+    for (let i = 0; i < mockTransactions.length; i += 1) {
+      it(`should return ${roundToSmallestCurrencyItemExpectations[i]}`, () => {
+        const result = calculations.roundToSmallestCurrencyItem(currencyForRounding[i]);
+        expect(result).to.equal(roundToSmallestCurrencyItemExpectations[i]);
+      });
+    }
+  });
 }
 
 function applicableForFeeAmountTest() {
-    describe('test for amount applicable for a fee', () => {
-        for (let i = 0; i < mockTransactions.length; i++) {
-            it('should return ' + weeklyApplicableAmountExpectations[i], () => {
-                const result = calculations.weeklyApplicableAmount(mockTransactions[i], currentWeeklyTotals[i], cashOutSettingsNatural);
-                expect(result).to.equal(weeklyApplicableAmountExpectations[i]);
-            });
-        }
-    });
+  describe('test for amount applicable for a fee', () => {
+    for (let i = 0; i < mockTransactions.length; i += 1) {
+      it(`should return ${weeklyApplicableAmountExpectations[i]}`, () => {
+        const result = calculations.weeklyApplicableAmount(mockTransactions[i], currentWeeklyTotals[i], cashOutSettingsNatural);
+        expect(result).to.equal(weeklyApplicableAmountExpectations[i]);
+      });
+    }
+  });
 }
 
 function weeklyCashOutAmountTest() {
-    delete require.cache[require.resolve('../calculations.js')];
-    const calculations = require('../calculations');
-    describe('test for current cash out amount for a given user in a given week', () => {
-        for (let i = 0; i < mockTransactions.length; i++) {
-            it('should return ' + currentWeeklyCashOutTotalExpectations[i], () => {
-                const result = calculations.currentWeeklyCashOutTotal(mockTransactions[i]);
-                expect(result).to.equal(currentWeeklyCashOutTotalExpectations[i]);
-            });
-        }
-    });
+  delete require.cache[require.resolve('../calculations.js')];
+  const calculations = require('../calculations');
+  describe('test for current cash out amount for a given user in a given week', () => {
+    for (let i = 0; i < mockTransactions.length; i += 1) {
+      it(`should return ${currentWeeklyCashOutTotalExpectations[i]}`, () => {
+        const result = calculations.currentWeeklyCashOutTotal(mockTransactions[i]);
+        expect(result).to.equal(currentWeeklyCashOutTotalExpectations[i]);
+      });
+    }
+  });
 }
+
+// tests, test names are pretty self-explanatory
+describe('Calculations test', () => {
+  feeForCashInTest();
+  feeForCashOutLegalTest();
+  feeForCashOutNaturalTest();
+  percentageCalculationTest();
+  currencyRoundingTest();
+  applicableForFeeAmountTest();
+  weeklyCashOutAmountTest();
+});
